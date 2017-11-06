8b1e6be6e4eb1f0b4f1f395aceeeb5a8d1c6ba3f
bkohrn <bkohrn@pdx.edu>
2017-11-06 14:48:35 -0800

    Update CallHap_HapCallr.py

--- CallHap_HapCallr.py
+++ CallHap_HapCallr.py
@@ -927,7 +927,7 @@ if __name__ == "__main__":
             source="CallHaps_HapCallr_%s" % progVersion, 
             commandLine=CommandStr, 
             baseHead=tmpVCF.headInfo, 
-            FormatBlock=[tmpVCF.headInfo["FORMAT"]])
+            FormatBlock=tmpVCF.headInfo["FORMAT"])
         output3.writeHeader(poolNames)
         output3.setFormat("RF")
         
